18fc862cfade64d391cbcf374ea4736b36cfd59c
teguhbudhi13 <50268422+teguhbudhi13@users.noreply.github.com>
2020-04-09 16:00:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/teguhbudhi13/products/app"
 	"github.com/teguhbudhi13/products/config"
 )
 
 func main() {
+	fmt.Println("Your API Now Is Published")
 	config := config.GetConfig()
 
 	app := &app.App{}
